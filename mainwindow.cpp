d6d5e4615fa6601cbaf5487d3608305f776d8b06
jeshuazeng <1906667878@qq.com>
2020-07-14 10:05:52 +0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,28 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtWebEngineWidgets/QWebEngineView>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QWebEngineView *pEngView = new QWebEngineView(this);
+        pEngView->setContextMenuPolicy(Qt::NoContextMenu);
+        //pEngView->load(QUrl("E:\\my_Documents\\echartTest\\testEcharts.html"));
+        pEngView->load(QUrl("file:///" + qApp->applicationDirPath() + "/htmlEcharts/testEcharts.html"));
+
+        pEngView->show();
+     setCentralWidget(pEngView);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
